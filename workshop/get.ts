419b034cef5643ab0e78772493a3d20038647289
clement belvisee <clement.belvisee@gmail.com>
2023-05-30 10:34:08 +0000

    add tp subsection comments

--- workshop/get.ts
+++ workshop/get.ts
@@ -1,6 +1,9 @@
+type pathArg = string | (string | number)[];
 const query = { response: { body: { data: { message: "hello", list: ["not me", "world"] } } } };
+
 {
-  const get = (obj: any, path: string | (string | number)[], defValue: any) => {
+  // What is optional chaining
+  const get = (obj: any, path: pathArg, defValue: any) => {
     if (!path) return undefined;
     const pathArray = Array.isArray(path) ? path : path.match(/([^[.\]])+/g) || [];
     const result = [...pathArray].reduce((prevObj: any, key: any) => prevObj && prevObj[key], obj);
@@ -12,7 +15,8 @@ const query = { response: { body: { data: { message: "hello", list: ["not me", "
 };
 
 {
-  const get = (obj: any) => (path: string | (string | number)[]) => (defValue: any) => {
+  // Add some spice
+  const get = (obj: any) => (path: pathArg) => (defValue: any) => {
     if (!path) return undefined;
     const pathArray = Array.isArray(path) ? path : path.match(/([^[.\]])+/g) || [];
     const result = [...pathArray].reduce((prevObj: any, key: any) => prevObj && prevObj[key], obj);
@@ -24,8 +28,9 @@ const query = { response: { body: { data: { message: "hello", list: ["not me", "
 }
 
 {
+  // Flipping functions
   const pipe = <ValueType>(...fns: CallableFunction[]) => (value: ValueType) => fns.reduce((current, callable) => callable(current), value);
-  const getOr = (defValue: any) => (path: string | (string | number)[]) => (obj: any) => {
+  const getOr = (defValue: any) => (path: pathArg) => (obj: any) => {
     if (!path) return undefined;
     const pathArray = Array.isArray(path) ? path : path.match(/([^[.\]])+/g) || [];
     const result = [...pathArray].reduce((prevObj: any, key: any) => prevObj && prevObj[key], obj);
